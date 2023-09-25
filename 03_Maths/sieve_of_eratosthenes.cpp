685c980348b2ee11e4b2f11df6b6873bfb998c4b
Vivek Kumar <118689020+vickvey@users.noreply.github.com>
2023-09-25 16:49:45 +0000

    modified sieve_of_eratosthenes.cpp

--- 03_Maths/sieve_of_eratosthenes.cpp
+++ 03_Maths/sieve_of_eratosthenes.cpp
@@ -42,29 +42,3 @@ int main(){
   }
   return 0;
 }
-
-// Explanation of Sieve of Eratosthenes
-/**
- * @title Sieve of Eratosthenes
- * @details 
- * 1. Create a list of consecutive integers from 2 to n: (2, 3, 4, ..., n).
- * 2. Initially, let p equal 2, the smallest prime number.
- * 3. Enumerate the multiples of p by counting in increments of p from 2p to n, and mark them 
- *    in the list (these will be 2p, 3p, 4p, ...; the p itself should not be marked).
- * 4. Find the first number greater than p in the list that is not marked. If there was no such
- *    number, stop. Otherwise, let p now equal this new number (which is the next prime), and
- *    repeat from step 3.
- * 5. When the algorithm terminates, the numbers remaining not marked in the list are all the
- *    primes below n.
- * 
- * The main idea here is that every value of p we choose, we mark all multiples of p as
- * composite (not prime). This way, when we iterate through all values of p, we will only
- * be left with prime numbers.
- * 
- * The algorithm can be improved further by observing that all primes are of the form 6k ± 1,
- * with the exception of 2 and 3. This is because all integers can be expressed as (6k + i)
- * for some integer k and for i = −1, 0, 1, 2, 3, or 4; 2 divides (6k + 0), (6k + 2), (6k + 4);
- * and 3 divides (6k + 3). So a more efficient method is to test if n is divisible by 2 or 3,
- * then to check through all the numbers of form 6k ± 1.
- * 
- */
