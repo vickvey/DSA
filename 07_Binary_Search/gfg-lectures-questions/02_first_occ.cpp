3a68c0480aed48d100a769ff40d64178b14f07a6
vickvey <vivek22@iiserb.ac.in>
2023-12-21 12:33:17 +0530

    added some files

--- 07_Binary_Search/gfg-lectures-questions/02_first_occ.cpp
+++ 07_Binary_Search/gfg-lectures-questions/02_first_occ.cpp
@@ -9,10 +9,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int first_occ(int key, int n, const int arr[]) {
+    int lo = 0;
+    int hi = n-1;
+
+    while(lo <= hi) {
+        int mid = lo + (hi - lo)/2;
+        
+    }
+}
+
+void solve() {
+    // question here
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    
+    int t = 0;
+    cin >> t;
+
+    while(t--) solve();
     return 0;
 }
